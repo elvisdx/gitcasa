82b9232eee935c76807a38f6b55c9de37684f546
Elvis Aleluia <elvis.11souza@gmail.com>
2018-03-25 00:46:32 -0300

    Commit Inicial

---
+++ angular-test/angular-zeroum/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'meu-primeiro-component',
+    template: `
+    <p>Meu Primeiro Component com Angular!</p>
+    `
+})
+
+export class MeuPrimeiroComponent { }
